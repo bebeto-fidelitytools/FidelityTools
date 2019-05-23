49c743f931fec4e6223ac7d0f6023c5ce593e658
David Christian Neri <davidneri@hotmail.com.ar>
2019-05-23 14:09:53 -0300

    Firs commit

---
+++ FidelityTools/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FidelityTools.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
